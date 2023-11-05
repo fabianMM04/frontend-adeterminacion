ae37f9fb416796f0992aa49ea124d3e9bc19c111
FabianMM <fabianmm04@gmail.com>
2023-11-05 18:07:13 -0500

    update upload file

--- src/app/modules/dashboard/components/historicoseisonce/historicoseisonce.component.ts
+++ src/app/modules/dashboard/components/historicoseisonce/historicoseisonce.component.ts
@@ -1,97 +1,110 @@
-import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HistoricoseisonceService } from '../../../../services/historicoseisonce.service';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { MatDialog, MatDialogConfig } from '@angular/material';
-import { HistoricoseisonceformComponent } from '../historicoseisonceform/historicoseisonceform.component';
-
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { HistoricoseisonceService } from "../../../../services/historicoseisonce.service";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { MatDialog, MatDialogConfig } from "@angular/material";
+import { HistoricoseisonceformComponent } from "../historicoseisonceform/historicoseisonceform.component";
 
 @Component({
-  selector: 'app-historicoseisonce',
-  templateUrl: './historicoseisonce.component.html',
-  styleUrls: ['./historicoseisonce.component.css']
+  selector: "app-historicoseisonce",
+  templateUrl: "./historicoseisonce.component.html",
+  styleUrls: ["./historicoseisonce.component.css"],
 })
 export class HistoricoseisonceComponent implements OnInit {
   public identity;
-  displayedColumns: string[] = ['REFERENCIA','REF_CATASTRAL', 'No_RESOLUCION', 'No_EXPEDIENTE', 'FECHA','VIG_DETERMINADAS', 'GRUPO','NOTIFICADO_DEVUELTO', 'BUSQUEDA', 'TOTAL_DETERMINADO', 'NO_IMAGE_SCANED','actions', 'new'];
+  displayedColumns: string[] = [
+    "status",
+    "REFERENCIA",
+    "REF_CATASTRAL",
+    "No_RESOLUCION",
+    "No_EXPEDIENTE",
+    "FECHA",
+    "VIG_DETERMINADAS",
+    "GRUPO",
+    "NOTIFICADO_DEVUELTO",
+    "BUSQUEDA",
+    "TOTAL_DETERMINADO",
+    "NO_IMAGE_SCANED",
+    "actions",
+    "new",
+  ];
   dataSource = new MatTableDataSource();
 
   formGroup: FormGroup;
-  constructor( private historicoService: HistoricoseisonceService,
+  constructor(
+    private historicoService: HistoricoseisonceService,
     private route: ActivatedRoute,
     private router: Router,
-    private dialog: MatDialog) { }
+    private dialog: MatDialog
+  ) {}
 
-    ngOnInit() {
-      this.historicoService.list_historico().subscribe( 
-        res => {
-          this.dataSource.data = res.historico;
-      }, error => {
-        console.log(<any> error);
+  ngOnInit() {
+    this.historicoService.list_historico().subscribe(
+      (res) => {
+        this.dataSource.data = res.historico;
+      },
+      (error) => {
+        console.log(<any>error);
       }
-      
-      );
-    }
-  
-    initForm(){
-      this.formGroup = new FormGroup({
-        filtro: new FormControl(''),
-      })
-    }
+    );
+  }
 
-    ngDoCheck() {
-      this.identity = localStorage.getItem('user');
-    }
-  
-    applyFilter(event: Event) {
-      const filterValue = (event.target as HTMLInputElement).value;
-      this.dataSource.filter = filterValue.trim().toLowerCase();
-    }
+  initForm() {
+    this.formGroup = new FormGroup({
+      filtro: new FormControl(""),
+    });
+  }
 
-    onEdit(element){
-      this.resetForm();
-      this.openForm();
-      if(element) {
-        this.historicoService.selected = element;
-      }
-    }
-  
-    onDelete(id:string){
-      console.log("id:", id)
-      this.historicoService.delete_historico(id).subscribe(
-        resp => {
-          console.log(resp)
-          this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=>
-          this.router.navigate(["/menu/historicosseisonce"]));
-        }
-      );
-    }
+  ngDoCheck() {
+    this.identity = localStorage.getItem("user");
+  }
 
-    openForm(): void {
-      const dialogConfig = new MatDialogConfig();
-      dialogConfig.data = {
-        title: "modal"
-      };
-      dialogConfig.autoFocus = true;
-      
-      this.dialog.open(HistoricoseisonceformComponent, dialogConfig);
-    }
-  
-    resetForm(): void {
-      this.historicoService.selected._id = null;
-      this.historicoService.selected.REF_CATASTRAL = '';
-      this.historicoService.selected.No_RESOLUCION = '';
-      this.historicoService.selected.No_EXPEDIENTE = '';
-      this.historicoService.selected.FECHA = '';
-      this.historicoService.selected.VIG_DETERMINADAS = '';
-      this.historicoService.selected.NOTIFICADO_DEVUELTO = '';
-      this.historicoService.selected.BUSQUEDA = '';
-      this.historicoService.selected.TOTAL_DETERMINADO = '';
-      this.historicoService.selected.REFERENCIA = '';
-      this.historicoService.selected.GRUPO = '';
-      this.historicoService.selected.NO_IMAGE_SCANED = '';
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
+  onEdit(element) {
+    this.resetForm();
+    this.openForm();
+    if (element) {
+      this.historicoService.selected = element;
     }
+  }
 
+  onDelete(id: string) {
+    console.log("id:", id);
+    this.historicoService.delete_historico(id).subscribe((resp) => {
+      console.log(resp);
+      this.router
+        .navigateByUrl("/", { skipLocationChange: true })
+        .then(() => this.router.navigate(["/menu/historicosseisonce"]));
+    });
+  }
+
+  openForm(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      title: "modal",
+    };
+    dialogConfig.autoFocus = true;
+
+    this.dialog.open(HistoricoseisonceformComponent, dialogConfig);
+  }
+
+  resetForm(): void {
+    this.historicoService.selected._id = null;
+    this.historicoService.selected.REF_CATASTRAL = "";
+    this.historicoService.selected.No_RESOLUCION = "";
+    this.historicoService.selected.No_EXPEDIENTE = "";
+    this.historicoService.selected.FECHA = "";
+    this.historicoService.selected.VIG_DETERMINADAS = "";
+    this.historicoService.selected.NOTIFICADO_DEVUELTO = "";
+    this.historicoService.selected.BUSQUEDA = "";
+    this.historicoService.selected.TOTAL_DETERMINADO = "";
+    this.historicoService.selected.REFERENCIA = "";
+    this.historicoService.selected.GRUPO = "";
+    this.historicoService.selected.NO_IMAGE_SCANED = "";
+  }
 }
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,55 +1,85 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { NavComponent } from './components/nav/nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-import {MatTableModule} from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatDialogModule} from '@angular/material/dialog';
-import { MatFileUploadModule } from 'angular-material-fileupload';
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { NavComponent } from "./components/nav/nav.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import {
+  MatFormFieldModule,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+} from "@angular/material/form-field";
+import { MatTableModule } from "@angular/material/table";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFileUploadModule } from "angular-material-fileupload";
 
+import { RconvenioComponent } from "./components/rconvenio/rconvenio.component";
+import { NembargoComponent } from "./components/nembargo/nembargo.component";
+import { MandamientopagoComponent } from "./components/mandamientopago/mandamientopago.component";
+import { ResolucionesComponent } from "./components/resoluciones/resoluciones.component";
+import { RunoydosComponent } from "./components/runoydos/runoydos.component";
+import { RembargoComponent } from "./components/rembargo/rembargo.component";
+import { ScontribuyenteComponent } from "./components/scontribuyente/scontribuyente.component";
+import { SarchivoComponent } from "./components/sarchivo/sarchivo.component";
+import { SabogadoComponent } from "./components/sabogado/sabogado.component";
 
-import { RconvenioComponent } from './components/rconvenio/rconvenio.component';
-import { NembargoComponent } from './components/nembargo/nembargo.component';
-import { MandamientopagoComponent } from './components/mandamientopago/mandamientopago.component';
-import { ResolucionesComponent } from './components/resoluciones/resoluciones.component';
-import { RunoydosComponent } from './components/runoydos/runoydos.component';
-import { RembargoComponent } from './components/rembargo/rembargo.component';
-import { ScontribuyenteComponent } from './components/scontribuyente/scontribuyente.component';
-import { SarchivoComponent } from './components/sarchivo/sarchivo.component';
-import { SabogadoComponent } from './components/sabogado/sabogado.component';
+import { RconvenioService } from "../../services/rconvenio.service";
+import { MandamientopagoformComponent } from "./components/mandamientopagoform/mandamientopagoform.component";
+import { NembargoformComponent } from "./components/nembargoform/nembargoform.component";
+import { RconvenioformComponent } from "./components/rconvenioform/rconvenioform.component";
+import { RembargoformComponent } from "./components/rembargoform/rembargoform.component";
+import { ResolucionesformComponent } from "./components/resolucionesform/resolucionesform.component";
+import { RunoydosformComponent } from "./components/runoydosform/runoydosform.component";
+import { SabogadoformComponent } from "./components/sabogadoform/sabogadoform.component";
+import { SarchivoformComponent } from "./components/sarchivoform/sarchivoform.component";
+import { ScontribuyenteformComponent } from "./components/scontribuyenteform/scontribuyenteform.component";
+import { NmensajeriaComponent } from "./components/nmensajeria/nmensajeria.component";
+import { NmensajeriaformComponent } from "./components/nmensajeriaform/nmensajeriaform.component";
+import { HistoricoComponent } from "./components/historico/historico.component";
+import { HistoricoformComponent } from "./components/historicoform/historicoform.component";
 
-import { RconvenioService} from '../../services/rconvenio.service';
-import { MandamientopagoformComponent } from './components/mandamientopagoform/mandamientopagoform.component';
-import { NembargoformComponent } from './components/nembargoform/nembargoform.component';
-import { RconvenioformComponent } from './components/rconvenioform/rconvenioform.component';
-import { RembargoformComponent } from './components/rembargoform/rembargoform.component';
-import { ResolucionesformComponent } from './components/resolucionesform/resolucionesform.component';
-import { RunoydosformComponent } from './components/runoydosform/runoydosform.component';
-import { SabogadoformComponent } from './components/sabogadoform/sabogadoform.component';
-import { SarchivoformComponent } from './components/sarchivoform/sarchivoform.component';
-import { ScontribuyenteformComponent } from './components/scontribuyenteform/scontribuyenteform.component';
-import { NmensajeriaComponent } from './components/nmensajeria/nmensajeria.component';
-import { NmensajeriaformComponent } from './components/nmensajeriaform/nmensajeriaform.component';
-import { HistoricoComponent } from './components/historico/historico.component';
-import { HistoricoformComponent } from './components/historicoform/historicoform.component';
+import { HistoricoseisonceComponent } from "./components/historicoseisonce/historicoseisonce.component";
+import { HistoricoseisonceformComponent } from "./components/historicoseisonceform/historicoseisonceform.component";
 
-import { HistoricoseisonceComponent } from './components/historicoseisonce/historicoseisonce.component';
-import { HistoricoseisonceformComponent } from './components/historicoseisonceform/historicoseisonceform.component';
-
-import { HistoricoochoComponent } from './components/historicoocho/historicoocho.component';
-import { HistoricoochoformComponent } from './components/historicoochoform/historicoochoform.component';
+import { HistoricoochoComponent } from "./components/historicoocho/historicoocho.component";
+import { HistoricoochoformComponent } from "./components/historicoochoform/historicoochoform.component";
 
 @NgModule({
-  declarations: [NavComponent, RconvenioComponent, NembargoComponent, MandamientopagoComponent, ResolucionesComponent, RunoydosComponent, RembargoComponent, ScontribuyenteComponent, SarchivoComponent, SabogadoComponent, MandamientopagoformComponent, NembargoformComponent, RconvenioformComponent, RembargoformComponent, ResolucionesformComponent, RunoydosformComponent, SabogadoformComponent, SarchivoformComponent, ScontribuyenteformComponent, NmensajeriaComponent, NmensajeriaformComponent, HistoricoComponent, HistoricoseisonceComponent, HistoricoochoComponent, HistoricoformComponent, HistoricoseisonceformComponent, HistoricoochoformComponent],
+  declarations: [
+    NavComponent,
+    RconvenioComponent,
+    NembargoComponent,
+    MandamientopagoComponent,
+    ResolucionesComponent,
+    RunoydosComponent,
+    RembargoComponent,
+    ScontribuyenteComponent,
+    SarchivoComponent,
+    SabogadoComponent,
+    MandamientopagoformComponent,
+    NembargoformComponent,
+    RconvenioformComponent,
+    RembargoformComponent,
+    ResolucionesformComponent,
+    RunoydosformComponent,
+    SabogadoformComponent,
+    SarchivoformComponent,
+    ScontribuyenteformComponent,
+    NmensajeriaComponent,
+    NmensajeriaformComponent,
+    HistoricoComponent,
+    HistoricoseisonceComponent,
+    HistoricoochoComponent,
+    HistoricoformComponent,
+    HistoricoseisonceformComponent,
+    HistoricoochoformComponent,
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
@@ -64,9 +94,27 @@ import { HistoricoochoformComponent } from './components/historicoochoform/histo
     MatInputModule,
     ReactiveFormsModule,
     MatDialogModule,
-    MatFileUploadModule
+    MatFileUploadModule,
   ],
   providers: [RconvenioService],
-  entryComponents:[MandamientopagoformComponent, NembargoformComponent, RconvenioformComponent, RembargoformComponent, ResolucionesformComponent, RunoydosformComponent, SabogadoformComponent, SarchivoformComponent, ScontribuyenteformComponent, NmensajeriaComponent, NmensajeriaformComponent, , HistoricoComponent, HistoricoseisonceComponent, HistoricoochoComponent, HistoricoformComponent, HistoricoseisonceformComponent, HistoricoochoformComponent]
+  entryComponents: [
+    MandamientopagoformComponent,
+    NembargoformComponent,
+    RconvenioformComponent,
+    RembargoformComponent,
+    ResolucionesformComponent,
+    RunoydosformComponent,
+    SabogadoformComponent,
+    SarchivoformComponent,
+    ScontribuyenteformComponent,
+    NmensajeriaComponent,
+    NmensajeriaformComponent,
+    HistoricoComponent,
+    HistoricoseisonceComponent,
+    HistoricoochoComponent,
+    HistoricoformComponent,
+    HistoricoseisonceformComponent,
+    HistoricoochoformComponent,
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
